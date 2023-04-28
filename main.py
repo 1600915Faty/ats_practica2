08f2561667ef1238444ea0a028d427e6a24a3f96
1600915Faty <115155576+1600915Faty@users.noreply.github.com>
2023-04-28 10:38:30 +0200

    can handle with big txt

--- main.py
+++ main.py
@@ -1,73 +1,67 @@
-# This is a sample Python script.
+import sys
+import os
 
-import threading
+def read_in_chunks(file_path, chunk_size=1024):
+    with open(file_path, 'r') as file:
+        while True:
+            data = file.read(chunk_size)
+            if not data:
+                break
+            yield data
 
-# Dividir els arxius en blocs
-def splitting(nom_arxiu, tam_bloc):
-    blocs = []
-    total_words = 0
-    with open(nom_arxiu, 'r') as f:
-        lines = f.readlines()
-        for i in range(0, len(lines), tam_bloc):
-            blocs = lines[i:i + tam_bloc]
-            total_words += sum(len(line.split()) for line in blocs)
+def mapper(file_path):
+    word_count = {}
+    for block in read_in_chunks(file_path):
+        block = block.replace(',', ' ').replace('.', ' ')
+        words = block.split()
+        for word in words:
+            word = word.lower()
+            if word not in word_count:
+                word_count[word] = 0
+            word_count[word] += 1
+    return word_count
 
-    return blocs, total_words
+def reducer(mapper_outputs):
+    word_count = {}
+    for mapper_output in mapper_outputs:
+        for word, count in mapper_output.items():
+            if word not in word_count:
+                word_count[word] = 0
+            word_count[word] += count
+    return word_count
 
-# Funció de map
-def map_func(bloc):
-    bloc_sense_comas = bloc.replace(',', ' ').replace('.', ' ')
-    paraules = [paraula.lower() for paraula in bloc_sense_comas.split()]
-    parells_clau_valor = [(paraula, 1) for paraula in paraules]
-    return parells_clau_valor
-
-# Funció de reduce
-def reduce_func(parell_clau_valor, num_caracters):
-    paraula, freq = parell_clau_valor[0], sum(parell_clau_valor[1])/num_caracters*100
-    return (paraula, freq)
-
-# Shuffle
-def shuffle(parells_clau_valor):
-    # Función Shuffle para unir todas las palabras y mostrar cuántas hay sin hacer la suma
-    parells_clau_valor_agrupats = {}
-    for tupla in parells_clau_valor:
-        for paraula, freq in tupla:
-            if paraula not in parells_clau_valor_agrupats:
-                parells_clau_valor_agrupats[paraula] = []
-            parells_clau_valor_agrupats[paraula].append(freq)
-    return parells_clau_valor_agrupats
-def main(nom_arxiu, tam_bloc):
-    blocs = []
-    total_words = 0
-    # Dividir els arxius en blocs
-    #for nom_arxiu in noms_arxius:
-     #   blocs, total_words = splitting(nom_arxiu, tam_bloc)
-    blocs, total_words = splitting(nom_arxiu, tam_bloc)
-    # Crear threads per a la funció de map
-    parells_clau_valor = []
-    threads_map = []
-    for bloc in blocs:
-        t = threading.Thread(target=lambda q, arg1: q.append(map_func(arg1)), args=(parells_clau_valor, bloc))
-        t.start()
-        threads_map.append(t)
-        for t in threads_map:
-            t.join()
-    # Crear threads per a la funció de shuffle
-    parells_clau_valor_agrupats = shuffle(parells_clau_valor)
-    # Executar la funció de reduce
-    parells_clau_valor_reduits = []
-    for paraula, freqs in parells_clau_valor_agrupats.items():
-        parells_clau_valor_reduits.append(reduce_func((paraula, freqs), total_words))
-    return parells_clau_valor_reduits
 if __name__ == '__main__':
-    noms_arxius = ["ArcTecSw_2023_BigData_Practica_Part1_Sample"]
-    tam_bloc = 1000
-        
+    #input_files = sys.argv[1:]
+    input_files = ["ArcTecSw_2023_BigData_Practica_Part1_Sample"]
+    mapper_outputs = []
+    for input_file in input_files:
+        mapper_output = mapper(input_file)
+        mapper_outputs.append(mapper_output)
+    word_count = reducer(mapper_outputs)
+    for input_file, file_word_count in zip(input_files, mapper_outputs):
+        print(f"{input_file}:")
+        for word, count in file_word_count.items():
+            frequency = (count / sum(file_word_count.values())) * 100
+            print(f"{word} : {frequency:.2f}%")
+        print()
+    """"
     for i in noms_arxius:
         resultats = main(i, tam_bloc)
         print(i + ":")
         for resultat in resultats:
+            print(resultat[0] + ": " + str(round(resultat[1], 2)) + "%")
+    """
+    """
+    # Lista de palabras aleatorias en inglés
+    words = ['car', 'apple', 'house', 'dog']
 
-            print(resultat[0] + ": " + str(round(resultat[1],2)) + "%")
-
+    # Crear un archivo de texto y escribir las palabras aleatorias
+    with open('words2.txt', 'w') as file:
+        for i in range(50000000):  # se generan 5000 líneas de 20 palabras cada una
+            line = ''
+            for j in range(20):
+                word = random.choice(words)
+                line += word + ' '
+            file.write(line.strip() + '\n')
+            """
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

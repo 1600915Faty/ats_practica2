756c70e12409f4ba40b456d5c9a167ba689afd75
1600915Faty <115155576+1600915Faty@users.noreply.github.com>
2023-05-03 10:56:52 +0200

    Need implementation of shuffle

--- main.py
+++ main.py
@@ -1,57 +1,77 @@
 import sys
-import os
-from multiprocessing import Pool
+import multiprocessing as mp
 
-def read_in_chunks(file_path, chunk_size=100):
-    with open(file_path, 'r') as file:
+def file_chunks(filename, chunk_size=1024*1024):
+    """Genera bloques de líneas del archivo."""
+    with open(filename, 'r', encoding='utf-8') as f:
         while True:
-            lines = file.readlines(chunk_size)
+            lines = f.readlines(chunk_size)
             if not lines:
                 break
-            yield ' '.join(lines)
+            yield lines
 
-def mapper(file_path, chunk_size=100):
-    word_count = {}
-    for block in read_in_chunks(file_path, chunk_size):
-        block = block.replace(',', ' ').replace('.', ' ')
-        words = block.split()
+def map_func(lines):
+    """Cuenta la frecuencia de cada palabra en un bloque de líneas."""
+    freq = {}
+    total = 0
+    for line in lines:
+        line = line.replace(',', ' ').replace('.', ' ')
+        words = line.strip().lower().split()
+
         for word in words:
-            word = word.lower()
-            if word not in word_count:
-                word_count[word] = 0
-            word_count[word] += 1
-    return word_count
+            freq[word] = freq.get(word, 0) + 1
+            total += 1
+    return freq
+    #return [(word, count/total*100) for word, count in freq.items()]
 
-def reducer(mapper_outputs):
-    word_count = {}
-    for mapper_output in mapper_outputs:
-        for word, count in mapper_output.items():
-            if word not in word_count:
-                word_count[word] = 0
-            word_count[word] += count
-    return word_count
+def reduce_func(results):
+    """Combina los resultados de varios bloques de líneas."""
+    freq = {}
+    total = 0
+    for block in results:
+        for name,num in block.items():
+            freq[name] = freq.get(name, 0) + num
+            total += num
+
+    return [(word, freq[word]/total*100) for word in sorted(freq.keys())]
+
+def run_mapreduce(filenames, num_processes):
+    # Crea un objeto Pool con el número de procesos especificado
+    pool = mp.Pool(num_processes)
+
+    # Crea una lista de tareas
+    tasks = [(filename, chunk) for filename in filenames for chunk in file_chunks(filename)]
+
+    # Ejecuta las tareas en paralelo
+    results = []
+    for task in tasks:
+        filename, chunk = task
+        result = map_func(chunk)
+        results.append((filename, result))
+
+    # Agrupa los resultados por archivo y combina los resultados de cada archivo
+    file_results = {}
+    for filename, result in results:
+        if filename not in file_results:
+            file_results[filename] = []
+        file_results[filename].append(result)
+    final_results = {}
+    for filename, results in file_results.items():
+        final_results[filename] = reduce_func(results)
+
+    # Muestra los resultados por pantalla
+    for filename in filenames:
+        print(f"{filename}:")
+        for word, freq in final_results[filename]:
+            print(f"{word} : {freq:.2f}%")
 
 if __name__ == '__main__':
-    # Obtener los argumentos de entrada
-    input_files = ["words2.txt"]
-    num_processes = 8  # Establecer el número de procesos
+    # Parsea los argumentos de la línea de comandos
+    #filenames = sys.argv[1:-1]
+    #num_processes = int(sys.argv[-1])
 
-    # Lanzar múltiples procesos utilizando la biblioteca multiprocessing
-    with Pool(num_processes) as p:
-        # Llamar al método mapper para cada archivo de entrada
-        mapper_outputs = p.starmap(mapper, [(f, 100) for f in input_files])
-
-    # Reducir la salida del mapper
-    word_count = reducer(mapper_outputs)
-
-    # Imprimir los resultados
-    for input_file, file_word_count in zip(input_files, mapper_outputs):
-        print(f"{input_file}:")
-        for word, count in file_word_count.items():
-            frequency = (count / sum(file_word_count.values())) * 100
-            print(f"{word} : {frequency:.2f}%")
-        print()
-
+    # Ejecuta el MapReduce
+    run_mapreduce(["words2.txt"], 8)
     """"
     #input_files = sys.argv[1:]
     for i in noms_arxius:

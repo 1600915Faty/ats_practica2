8792b175cf826ff1cd4a28433014c233408be58f
Loann-ca <91115629+Loann-ca@users.noreply.github.com>
2023-05-15 15:47:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+AUTORS
+LOANN COSANO ALCUDIA - 1600898
+ISMAEL FATY TOLOSA - 1600915
+
+"""
 import random
 import sys
 import multiprocessing
@@ -10,6 +16,7 @@ def read_file_optionals(filename):
     """Función auxiliar para leer el contenido de un archivo."""
     with open(filename, 'r', encoding='utf-8') as f:
         return f.read()
+
 
 def map_function(lines):
     """Función Map que cuenta la frecuencia de las palabras en una lista de líneas."""
@@ -44,6 +51,8 @@ def shuffle_function(mapped_list):
                     results[name] = []
                 results[name].append(1)
     return results
+
+"""Funció que retorna el numero de paraules/lletres totals que hi ha per fer el %"""
 def countTotal(reduced):
     total=0
     for dicc in reduced:
@@ -51,6 +60,7 @@ def countTotal(reduced):
             total+=dicc[key]
     return total
 
+"""Funcion para fusionar los resultados de los dos archivos"""
 def files_fusion(results):
     final_dicctionarie={}
     for result in results:
@@ -69,88 +79,90 @@ def process_file(filename, num_processes, optional_mode):
     else:
         lines = read_file_optionals(filename)
         blocks = [lines[i:i + 800] for i in range(0, len(lines), 800)]  # dividir el archivo en bloques de 1000 líneas
-    """
+    """ LOOP LINEAL PER FER COMPARACIONS EN EL INFORME
     mapped=[]
     for block in blocks:
         mapped.append(map_function(block))
     """
     with multiprocessing.Pool(processes=num_processes) as pool:
         mapped = pool.map(map_function, blocks)
+
     # Obtener los resultados de todas las tareas
     shuffled = shuffle_function(mapped)
     with multiprocessing.Pool(processes=num_processes) as pool:
         result = pool.map(reduce_function, [shuffled])
+
+    """ LOOP LINEAL PER FER COMPARACIONS EN EL INFORME
+    result = []
+    for item in [shuffled]:
+        results = reduce_function(item)
+        result.append(results)
+    """
     total = countTotal(result)
     return result, total
 
 if __name__ == '__main__':
 
     # parsear los argumentos de línea de comandos
-    num_processes = 8  # valor por defecto
+    num_processes = 12  # valor por defecto
     optional_mode = 0  # valor por defecto
-    filenames = ["49MB.txt", "ArcTecSw_2023_BigData_Practica_Part1_Sample"]
+    filenames = []
 
-    """
+    #Leer los parametros
     for arg in sys.argv[1:]:
         if arg.startswith('-p'):
-            num_processes = int(arg[2:])
+            num_processes = int(arg[3:])
         elif arg.startswith('-o'):
             optional_mode = int(arg[3:])
         else:
-            filenames.append(arg)
-    """
+            filenames.append("/data/" + arg)
+
     #for optional_mode 0 and 1
     if optional_mode!=2:
         # ejecutar el Map-Reduce en cada archivo
         for filename in filenames:
-            start = time.time()
+            #start = time.time()
             result, total = process_file(filename, num_processes, optional_mode)
-            final = time.time()
+            #final = time.time()
 
             # imprimir el resultado
-            print(f'{filename}:')
+            print(f'{filename[6:]}:')
             for dicc in result:
                 for word, count in sorted(dicc.items(), key=lambda x: x[1], reverse=True):
                     percentage = 100.0 * count / total
                     print(f'{word} : {percentage:.2f}%')
             print()
-            print(final - start)
-            print()
+            #print(final - start)
+            #print()
     #for optional mode 2, mix different files
     else:
-        start = time.time()
+        #start = time.time()
         results=[]
         totals = 0
         print('Files mixed:')
+
         for filename in filenames:
-            print(f'{filename}')
+            print(f'{filename[6:]}')
             result, total = process_file(filename, num_processes, optional_mode)
             totals+=total
             results.append(result)
         final_result= files_fusion(results)
-        final = time.time()
+        #final = time.time()
         print('Results:')
         for dicc in final_result:
             for word, count in sorted(dicc.items(), key=lambda x: x[1], reverse=True):
                 percentage = 100.0 * count / totals
                 print(f'{word} : {percentage:.2f}%')
         print()
-        print(final - start)
-    """
-    #input_files = sys.argv[1:]
-    for i in noms_arxius:
-        resultats = main(i, tam_bloc)
-        print(i + ":")
-        for resultat in resultats:
-            print(resultat[0] + ": " + str(round(resultat[1], 2)) + "%")
-    """
+        #print(final - start)
+
     """
     # Lista de palabras aleatorias en inglés
     words = ['car', 'apple', 'house', 'dog', 'pepa', 'coco']
 
     # Crear un archivo de texto y escribir las palabras aleatorias
     with open('words7.txt', 'w') as file:
-        for i in range(3200000):  # se generan 5000 líneas de 20 palabras cada una
+        for i in range(5200000):  # se generan 5000 líneas de 20 palabras cada una
             line = ''
             for j in range(20):
                 word = random.choice(words)
